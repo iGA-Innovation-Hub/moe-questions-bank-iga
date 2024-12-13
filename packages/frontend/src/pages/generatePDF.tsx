f7ed1baecf55c55f38ea6536fd833360c00de44f
Almutawaj <foetheuniversity2022@gmail.com>
2024-12-13 20:35:01 +0300

    PDF File  Formatted

--- packages/frontend/src/pages/generatePDF.tsx
+++ packages/frontend/src/pages/generatePDF.tsx
@@ -2,6 +2,9 @@ import { jsPDF } from "jspdf";
 import "jspdf-autotable";
 import { Exam, Section, SubSection, Content, Question, Exercise, ReadingQuestions } from "./InterfacesPDF";
 
+const marginLeft = 20;
+const marginRight = 190; // Adjust according to the document size
+
 export const generateExamPDF = (jsonString: string) => {
     const jsonContent = jsonString.substring(
         jsonString.indexOf("{"),
@@ -31,29 +34,54 @@ function addFooter(doc: jsPDF, title: string): void {
     for (let i = 1; i <= pageCount; i++) {
         doc.setPage(i);
         doc.setFont("Times", "italic", 10);
-        doc.text(`${title}`, 10, 290);
-        doc.text(`Page ${i} of ${pageCount}`, 200, 290, { align: "right" });
+        doc.text(`${title}`, marginLeft, 290);
+        doc.text(`Page ${i} of ${pageCount}`, 195, 290, { align: "right" });
     }
 }
 
 function addExamInfo(doc: jsPDF, exam: Exam, yPosition: number): void {
-    doc.setFont("Times", "bold", 18);
-    doc.text(exam.title, 105, yPosition, { align: "center" });
+    // Ministry header
+    doc.setFont("Times", "bold", 12);
+    doc.text("KINGDOM OF BAHRAIN", 105, yPosition, { align: "center" });
+    yPosition += 8;
+    doc.text("MINISTRY OF EDUCATION", 105, yPosition, { align: "center" });
+    yPosition += 8;
+    doc.text("Internal Exams Section", 105, yPosition, { align: "center" });
+    yPosition += 8;
+    doc.text("SECOND SEMESTER FINAL EXAM 2024-2025", 105, yPosition, { align: "center" });
 
-    yPosition += 10;
+    // Add course info
+    yPosition += 12;
     doc.setFont("Times", "normal", 12);
-    doc.text(`Total Marks: ${exam.total_marks}`, 20, yPosition);
-    doc.text(`Time: ${exam.time}`, 160, yPosition);
+    doc.text(`COURSE NAME: English Language`, marginLeft, yPosition);
+    yPosition += 8;
+    doc.text(`COURSE CODE: ENG102`, marginLeft, yPosition);
+    doc.text(`Total Marks: ${exam.total_marks}`, 160, yPosition); 
+    yPosition += 8;
+    doc.text(`Track: Unified Tracks`, marginLeft, yPosition);
+    doc.text(`Time: 2 Hours`, 160, yPosition);
+
+    // Divider line
+    yPosition += 8;
+    const line = "=".repeat(60);
+    doc.setFont("Times", "normal", 10);
+    doc.text(line, marginLeft, yPosition);
 }
 
+
+
 function addSection(doc: jsPDF, section: Section, yPosition: number): number {
-    if (yPosition > 200) {
+    if (yPosition > 195 || section.title.includes('Reading') || section.title.includes('Writing')) {
         doc.addPage();
         yPosition = 30;
     }
 
+    if(section.title.includes('Listening')) {
+        yPosition += 50; 
+    }
+
     doc.setFont("Times", "bold", 14);
-    doc.text(`Part ${section.part}: ${section.title} (${section.total_marks} marks)`, 20, yPosition);
+    doc.text(`Part ${section.part}: ${section.title} (${section.total_marks} marks)`, marginLeft, yPosition);
     yPosition += 12;
 
     if (section.subsections && section.subsections.length > 0) {
@@ -72,14 +100,14 @@ function addSection(doc: jsPDF, section: Section, yPosition: number): number {
 }
 
 function addSubSection(doc: jsPDF, subsection: SubSection, yPosition: number): number {
-    if (yPosition > 200) {
+    if (yPosition > 195) {
         doc.addPage();
         yPosition = 30;
     }
 
     doc.setFont("Times", "italic", 12);
-    doc.text(`Subsection ${subsection.subsection}: ${subsection.title} (${subsection.marks} marks)`, 20, yPosition);
-    yPosition += 10;
+    doc.text(`Subsection ${subsection.subsection}: ${subsection.title} (${subsection.marks} marks)`, marginLeft, yPosition);
+    yPosition += 12;
 
     if (subsection.content) {
         yPosition = addContent(doc, subsection, yPosition);
@@ -91,17 +119,11 @@ function addSubSection(doc: jsPDF, subsection: SubSection, yPosition: number): n
 }
 
 function addContent(doc: jsPDF, section: SubSection | Section, yPosition: number): number {
-
-    // it may be a SubSection or a Section
     const content = section.content as Content;
 
     if (content.passage && !section.title.includes("Listening")) {
-        yPosition = addTextBlock(doc, content.passage, yPosition + 10, "Passage:\n\n") + 20;
+        yPosition = addTextBlock(doc, content.passage, yPosition + 10, "Please read the following passage and answer the following questions:") + 20;
     }
-
-    // if (content.dialogue) {
-    //     yPosition = addTextBlock(doc, content.dialogue, yPosition, "Dialogue:") + 10;
-    // }
 
     if (Array.isArray(content.questions) && !content.questions.every((q: any) => q.word_limit)) {
         yPosition = addGeneralQuestions(doc, content.questions, yPosition);
@@ -109,7 +131,7 @@ function addContent(doc: jsPDF, section: SubSection | Section, yPosition: number
         const questions = content.questions as ReadingQuestions;
 
         if (questions["multiple-choice"]) {
-            yPosition = addGeneralQuestions(doc, questions["multiple-choice"], yPosition + 20, "Multiple Choice:") + 20;
+            yPosition = addGeneralQuestions(doc, questions["multiple-choice"], yPosition + 20, "A. Multiple Choice:") + 20;
         }
         if (questions["true-false"]) {
             yPosition = addTrueFalseQuestions(doc, questions["true-false"], yPosition) + 10;
@@ -131,41 +153,45 @@ function addContent(doc: jsPDF, section: SubSection | Section, yPosition: number
 }
 
 function addTextBlock(doc: jsPDF, text: string, yPosition: number, heading: string): number {
+
     doc.setFont("Times", "bold", 12);
-    doc.text(heading, 20, yPosition);
-    yPosition += 6;
+    doc.text(heading, marginLeft, yPosition);
+    yPosition += 12;
 
     doc.setFont("Times", "normal", 11);
-    const lines = doc.splitTextToSize(text, 170);
-    doc.text(lines, 20, yPosition);
+    const lines = doc.splitTextToSize(text, marginRight - marginLeft);
+    doc.text(lines, marginLeft, yPosition);
     return yPosition + lines.length * 6 + 30;
 }
 
 function addGeneralQuestions(doc: jsPDF, questions: Question[], yPosition: number, heading?: string): number {
-
     if (heading) {
-        if (yPosition > 200) {
+        if (yPosition > 195) {
             doc.addPage();
             yPosition = 30;
             doc.setFont("Times", "bold", 12);
-            doc.text(heading, 20, yPosition);
-            yPosition += 10;
+            doc.text(heading, marginLeft, yPosition);
+            yPosition += 12;
         }
     }
 
     questions.forEach((q, index) => {
-        if (yPosition > 200) {
+        if (yPosition > 195) {
             doc.addPage();
             yPosition = 30;
         }
 
         doc.setFont("Times", "normal", 11);
-        doc.text(`${index + 1}. ${q.question || q.sentence}`, 20, yPosition);
-        yPosition += 13;
+        const questionText = `${index + 1}. ${q.question || q.sentence}`;
+        const lines = doc.splitTextToSize(questionText, marginRight - marginLeft);
+        doc.text(lines, marginLeft, yPosition);
+        yPosition += lines.length * 6 + 5;
 
         q.options?.forEach((option, i) => {
-            doc.text(`   ${String.fromCharCode(65 + i)}. ${option}`, 20, yPosition);
-            yPosition += 8;
+            const optionText = `   ${String.fromCharCode(65 + i)}. ${option}`;
+            const optionLines = doc.splitTextToSize(optionText, marginRight - marginLeft);
+            doc.text(optionLines, marginLeft, yPosition);
+            yPosition += optionLines.length * 6 + 5;
         });
     });
 
@@ -174,17 +200,17 @@ function addGeneralQuestions(doc: jsPDF, questions: Question[], yPosition: numbe
 
 function addTrueFalseQuestions(doc: jsPDF, questions: Question[], yPosition: number): number {
     doc.setFont("Times", "bold", 12);
-    doc.text("True/False Questions:", 20, yPosition);
-    yPosition += 13;
+    doc.text("B. True/False Questions:", marginLeft, yPosition);
+    yPosition += 12;
 
     questions.forEach((q, index) => {
-        if (yPosition > 200) {
+        if (yPosition > 210) {
             doc.addPage();
             yPosition = 30;
         }
 
         doc.setFont("Times", "normal", 11);
-        doc.text(`${index + 1}. ${q.statement} (   )`, 20, yPosition);
+        doc.text(`${index + 1}. ${q.statement} (   )`, marginLeft, yPosition);
         yPosition += 13;
     });
 
@@ -198,20 +224,18 @@ function shuffleArray(A: Question[]): void {
     }
 }
 
-
 function addVocabularyMatching(doc: jsPDF, vocabList: Question[], yPosition: number): number {
     doc.setFont("Times", "bold", 12);
-    doc.text("Vocabulary Matching:", 20, yPosition);
-    yPosition += 10;
+    doc.text("C. Vocabulary Matching:", marginLeft, yPosition);
+    yPosition += 12;
 
-    let list_of_vocab = '';
-    shuffleArray(vocabList); 
-    vocabList.forEach((vocab, index) => {
-       list_of_vocab += `${index + 1}. ${(vocab as any).word}     `;
-    });
+    shuffleArray(vocabList);
 
+    const vocabWords = vocabList.map((vocab, index) => `${index + 1}. ${(vocab as any).word}`).join("    ");
+    const vocabLines = doc.splitTextToSize(vocabWords, marginRight - marginLeft);
     doc.setFont("Times", "bold", 11);
-    doc.text(list_of_vocab, 20, yPosition);
+    doc.text(vocabLines, marginLeft, yPosition);
+    yPosition += vocabLines.length * 6 + 10;
 
     vocabList.forEach((vocab, index) => {
         if (yPosition > 210) {
@@ -219,8 +243,10 @@ function addVocabularyMatching(doc: jsPDF, vocabList: Question[], yPosition: num
             yPosition = 30;
         }
         doc.setFont("Times", "normal", 11);
-        doc.text(`${index + 1}. __________________ ${(vocab as any).definition}`, 20, yPosition + 20);
-        yPosition += 10;
+        const definitionText = `${index + 1}. __________________ ${(vocab as any).definition }.`;
+        const definitionLines = doc.splitTextToSize(definitionText, marginRight - marginLeft);
+        doc.text(definitionLines, marginLeft, yPosition);
+        yPosition += definitionLines.length * 6 + 10;
     });
 
     return yPosition;
@@ -228,18 +254,20 @@ function addVocabularyMatching(doc: jsPDF, vocabList: Question[], yPosition: num
 
 function addExercises(doc: jsPDF, exercises: Exercise[], yPosition: number): number {
     doc.setFont("Times", "bold", 12);
-    doc.text("Exercises:", 20, yPosition);
-    yPosition += 15;
+    doc.text("D. Exercises:", marginLeft, yPosition);
+    yPosition += 12;
 
     exercises.forEach((exercise, index) => {
-        if (yPosition > 200) {
+        if (yPosition > 195) {
             doc.addPage();
             yPosition = 30;
         }
 
         doc.setFont("Times", "normal", 11);
-        doc.text(`${index + 1}. ${exercise.question}`, 20, yPosition);
-        yPosition += 12;
+        const questionText = `${index + 1}. ${exercise.question}`;
+        const questionLines = doc.splitTextToSize(questionText, marginRight - marginLeft);
+        doc.text(questionLines, marginLeft, yPosition);
+        yPosition += 15;
     });
 
     return yPosition;
@@ -247,20 +275,26 @@ function addExercises(doc: jsPDF, exercises: Exercise[], yPosition: number): num
 
 function addWritingQuestions(doc: jsPDF, writingQuestions: Question[], yPosition: number): number {
     writingQuestions.forEach((q, index) => {
+        if (yPosition > 195) {
+            doc.addPage();
+            yPosition = 30;
+        }
         doc.setFont("Times", "bold", 12);
-        doc.text(`Writing Question ${index + 1}:`, 20, yPosition);
-        yPosition += 10;
-
+        doc.text(`Writing Question ${index + 1}:`, marginLeft, yPosition);
+        yPosition += 12;
+        
         doc.setFont("Times", "normal", 11);
-        doc.text(q.prompt as string, 20, yPosition);
-        yPosition += 10;
+        const questionText = `${q.prompt as string} \n\nWord Limit: (${q.word_limit})`;
+        const questionLines = doc.splitTextToSize(questionText, marginRight - marginLeft);
+        doc.text(questionLines, marginLeft, yPosition);
+        yPosition += questionLines.length * 6 + 10;
 
-        doc.text(`Word Limit: ${q.word_limit}`, 20, yPosition);
-        yPosition += 10;
+        // Add space for the answer
+        const answerLines = Array(27).fill("").map(() => "_".repeat(60)).join("\n");
+        const answerYPosition = doc.splitTextToSize(answerLines, marginRight - marginLeft);
+        doc.text(answerYPosition, marginLeft, yPosition);
+        yPosition += answerYPosition.length * 6 + 20;
     });
 
     return yPosition;
 }
-
-
-
